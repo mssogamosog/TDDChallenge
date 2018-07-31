544a7d304abcf6a816d6f70966213b43b14d725f
Michael Sogamoso <mssg_2211@hotmail.com>
2018-07-31 15:31:29 -0500

    change implementatation to mergesort, emptyTestFailed

--- TDDChallenge2/ArraySort.cs
+++ TDDChallenge2/ArraySort.cs
@@ -12,35 +12,50 @@ namespace TDDChallenge2
         public static int[] Sort(int[] array)
         {
 
-            int length = array.Length;
-            if(length == 0)
-            {
-                return array;
-            }
-            else
-            {
-                int temp = array[0];
+            Quicksort(array, 0, array.Length - 1);
+            return array;
 
-                for (int i = 0; i < length; i++)
-                {
-                    for (int j = i + 1; j < length; j++)
-                    {
-                        if (array[i] > array[j])
-                        {
-                            temp = array[i];
-
-                            array[i] = array[j];
-
-                            array[j] = temp;
-                        }
-                    }
-                }
-
-                return array;
-            }
-            
         }
 
+        public static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
 
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+        }
     }
 }
