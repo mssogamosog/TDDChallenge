7b914f0481a3f508faf7bfeea4bc5a06e6df41c5
Michael Sogamoso <mssg_2211@hotmail.com>
2018-07-31 14:06:12 -0500

    test for sort method created

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDChallenge2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ArraySort_Is_Sorted()
+        {
+            //given
+            int[] toTest = new int[20] { 4, 8, 3, 7, 90, 11, 5, 0, 2, 5, 2, 5, 8, 1, 46, 1, 8, 3, 1, 56 };
+            int[] expectedResult = new int[20] { 0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 7, 8, 8, 8, 11, 46, 56, 90 };
+            //when
+            int[] resultFromFunction = ArraySort.Sort(toTest);
+            //then
+            Assert.IsNotNull(resultFromFunction);
+            Assert.IsTrue(resultFromFunction[0] == expectedResult[0]);
+            Assert.IsTrue(resultFromFunction[19] == expectedResult[0]);
+            Assert.IsTrue(resultFromFunction[9] == expectedResult[0]);
+            Assert.IsTrue(resultFromFunction[4] == expectedResult[0]);
+            Assert.IsTrue(resultFromFunction[14] == expectedResult[0]);
+        }
+    }
+}
